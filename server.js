1e4a2570ae12be1620b5f353f89fdedb6f69874e
Arham Malik <arhammalik900@yahoo.com>
2024-12-05 22:32:30 +0500

    commit 6

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use("/api", todosRouter);
 
